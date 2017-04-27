31d57776007f70e68e61a66f9d0981483c61a595
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-27 17:46:32 +0600

    Init

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  # get 'tasks/index'
+
   devise_for :users
   # get 'home/index'
 
   root 'home#index'
 
+  resources :tasks
   resources :home
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
