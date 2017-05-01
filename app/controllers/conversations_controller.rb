5f035848f4730cc8e968f9ad13e2d38e7a4f669a
Shabbir <shabbir@salahsoftwaresolution.com>
2017-05-01 15:52:07 +0600

    Action Cable

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,8 +4,8 @@ class ConversationsController < ApplicationController
 	def index
 		session[:conversations] ||= []
 
-  		@users = User.all.where.not(id: current_user)
-  		@conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
+    @users = User.all.where.not(id: current_user)
+  	@conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
 	end
 
 
