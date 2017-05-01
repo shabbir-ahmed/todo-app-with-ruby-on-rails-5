f8301c9658e9d909f2d363718077aa99f880397b
Shabbir <shabbir@salahsoftwaresolution.com>
2017-05-01 12:01:31 +0600

    Communication Channel

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://todo-with-rails-5.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://todo-with-rails-5.herokuapp.com', 'http://todo-with-rails-5.herokuapp.com' ]
+  config.action_cable.allowed_request_origins = [ 'https://todo-with-rails-5.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
