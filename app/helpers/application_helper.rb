6d4ed36133d08e611574f890dedb4947f510a4f7
Shabbir <shabbir@salahsoftwaresolution.com>
2017-05-01 12:42:57 +0600

    Communication Channel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+    def avatar_url(user)
+        gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+        "http://gravatar.com/avatar/#{gravatar_id}.png"
+    end
+
     def resource_name
         :user
     end
