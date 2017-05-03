f9a20493aeda1e587886a221c863c3d3eafc5969
Shabbir <shabbir@salahsoftwaresolution.com>
2017-05-03 16:21:00 +0600

    Activity

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   	def index
-  		@activities = PublicActivity::Activity.order("created_at desc")
+  		@activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user)
   	end
 end
