ff66d71026e8e188c17726a816ad9c90e40ea1bd
Shabbir <shabbir@salahsoftwaresolution.com>
2017-05-03 16:58:15 +0600

    Beautify

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,37 +17,18 @@
 // require turbolinks
 //= require_tree .
 
-$(function() {
-
-    $('#login-form-link').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#register-form-link').click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-
-});
-
 (function() {
-  $(document).on('click', '.toggle-window', function(e) {
-    e.preventDefault();
-    var panel = $(this).parent().parent();
-    var messages_list = panel.find('.messages-list');
+    $(document).on('click', '.toggle-window', function(e) {
+        e.preventDefault();
+        var panel = $(this).parent().parent();
+        var messages_list = panel.find('.messages-list');
 
-    panel.find('.panel-body').toggle();
-    panel.attr('class', 'panel panel-default');
+        panel.find('.panel-body').toggle();
+        panel.attr('class', 'panel panel-default');
 
-    if (panel.find('.panel-body').is(':visible')) {
-      var height = messages_list[0].scrollHeight;
-      messages_list.scrollTop(height);
-    }
-  });
+        if (panel.find('.panel-body').is(':visible')) {
+            var height = messages_list[0].scrollHeight;
+            messages_list.scrollTop(height);
+        }
+    });
 })();
