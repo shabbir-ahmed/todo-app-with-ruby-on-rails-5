4248ec5b4849aaad3670e84d0a9d01b07d7dcbef
Shabbir <shabbir@salahsoftwaresolution.com>
2017-05-05 16:09:36 +0600

    Active Admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -17,7 +17,8 @@ ActiveAdmin.register User do
 	filter :created_at
 
 	index do
-    	column :avatar
+		selectable_column
+    	id_column
     	column :full_name
     	column :email
     	column :location
@@ -29,7 +30,6 @@ ActiveAdmin.register User do
 
   	show do
 	    attributes_table do
-	      	row :avatar
      	 	row :full_name
      	 	row :email
      	 	row :location
@@ -38,5 +38,16 @@ ActiveAdmin.register User do
      	 	row :created_at
     	end
   	end
+
+  	form do |f|
+	    f.inputs "User Details" do
+	      f.input :full_name
+	      f.input :location
+	      f.input :email
+	      f.input :password
+	      f.input :password_confirmation
+	    end
+	    f.actions
+	end
 
 end
