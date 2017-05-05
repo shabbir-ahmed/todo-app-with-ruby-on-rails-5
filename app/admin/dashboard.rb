9ca3e8243391c1f357c3c98908265247ce212884
Shabbir <shabbir@salahsoftwaresolution.com>
2017-05-05 18:33:44 +0600

    Active Admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,11 +3,20 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
+
+    section "Recent Users" do
+        table_for User.order("created_at desc").limit(5) do
+          column :full_name
+          column :email
+          column "Registration Date", :created_at
+        end
+        strong { link_to "All Users", admin_users_path }
     end
 
     # Here is an example of a simple dashboard with columns and panels.
