1521d68516c3974dc16cedffba4eced633856287
Shabbir <shabbir@salahsoftwaresolution.com>
2017-05-05 12:00:26 +0600

    Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,22 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  Rails.application.routes.default_url_options[:host] = 'todo-with-rails-5.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'todo-with-rails-5.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["shabbir@salahsoftwaresolution.com"],
+    password: ENV["kobita@2014"]
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
