b2229d51f3a6590563e777f1ac42d343fc1ebaa6
Shabbir <shabbir@salahsoftwaresolution.com>
2017-05-07 17:38:23 +0600

    New Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,25 +1,22 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  # Rails.application.routes.default_url_options[:host] = 'todo-with-rails-5.herokuapp.com'
-  # config.action_mailer.default_url_options = { :host => 'todo-with-rails-5.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'todo-with-rails-5.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'todo-with-rails-5.herokuapp.com' }
 
-    # config.action_mailer.delivery_method = :smtp
-    # config.action_mailer.perform_deliveries = true
-    # config.action_mailer.raise_delivery_errors = false
-    # config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
-    config.action_mailer.default_url_options = {:host => 'todo-with-rails-5.herokuapp.com', :protocol => 'http'} #I've also tried it without ":protocol => 'http'"
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.smtp_settings = {
-        :address => "smtp.gmail.com",
-        :port => 587,
-        :authentication => :plain,
-        :enable_starttls_auto => true,
-        :user_name => 'shabbirahmed149@gmail.com',
-        :password => 'Thisis@1991'
-    }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["shabbir@salahsoftwaresolution.com"],
+    password: ENV["kobita@2014"]
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
