2dcda1ea1fa46cc9ba2e03fadfcd91ce6fa8f840
Shabbir <shabbir@salahsoftwaresolution.com>
2017-05-07 11:49:50 +0600

    New Update

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :full_name, :location, :email, :password, :password_confirmation
+permit_params :full_name, :location, :email, :role, :status, :password, :password_confirmation
 #
 # or
 #
@@ -13,7 +13,9 @@ permit_params :full_name, :location, :email, :password, :password_confirmation
 # end
 	menu priority: 3
 	filter :full_name
-	filter :email
+  filter :email
+  filter :role
+	filter :status
 	filter :location
 	filter :created_at
 
@@ -21,10 +23,12 @@ permit_params :full_name, :location, :email, :password, :password_confirmation
 		selectable_column
     	id_column
     	column :full_name
-    	column :email
+        column :email
+    	column :role
     	column :location
     	column :last_sign_in_at
-    	column :last_sign_in_ip
+        column :last_sign_in_ip
+    	column :status
     	column "Signup", :created_at
     	actions
   	end
@@ -32,21 +36,27 @@ permit_params :full_name, :location, :email, :password, :password_confirmation
   	show do
 	    attributes_table do
      	 	row :full_name
-     	 	row :email
+            row :email
+     	 	row :role
      	 	row :location
      	 	row :last_sign_in_at
-     	 	row :last_sign_in_ip
+            row :last_sign_in_ip
+     	 	row :status
      	 	row :created_at
     	end
   	end
 
   	form do |f|
 	    f.inputs "User Details" do
-	      f.input :full_name
-	      f.input :location
-	      f.input :email
-	      f.input :password
-	      f.input :password_confirmation
+            f.input :full_name
+            f.input :location
+            f.input :email
+            f.input :role, label: 'Role', as: :select, prompt: true, collection: [["Agent","Agent"],["None","None"]]
+            if f.object.new_record?
+                f.input :password
+                f.input :password_confirmation
+            end
+            f.input :status, label: 'Status', as: :select, prompt: true, collection: [["Active","Active"],["Deactive","Deactive"]]
 	    end
 	    f.actions
 	end
